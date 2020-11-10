b6e855e5eaaaf313e6f6550d619faf5dfa88ef6c
Ziad Al-Moqamar <ziad.almoqamar@gmail.com>
2020-11-11 01:48:21 +0200

    poping back typed name in textfield

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -8,6 +8,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  String cityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,12 +42,14 @@ class _CityScreenState extends State<CityScreen> {
                   ),
                   decoration: kTextFieldInputDecoration,
                   onChanged: (value){
-                    print(value);
+                    cityName = value;
                   },
                 ),
               ),
               FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text(
                   'Get Weather',
                   style: kButtonTextStyle,
